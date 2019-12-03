0a16a31ddd481040e0ee461bd300ce6b6a8176ae
wangzg_csd <wangzg_csd@si-tech.com.cn>
2019-12-03 11:49:53 +0800

    init repository

---
+++ creat-spring-boot/wzg/src/main/java/com/wzg/creat/user/model/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.wzg.creat.user.model.mapper;
+
+import com.wzg.creat.user.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
